13802bdb9a7f610811d744a5b4c301e00e64706c
Ezra <ezrakipkorir13@gmail.com>
2024-04-01 14:45:04 +0300

    Arithmetic solutions.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+// Write your code 
+let random = Math.floor(Math.random() * 10) + 1;
+
+let max = Math.max(10,12,14,16,18,20);
+
+let num3 = 20;
+let num4 =  8;
+let mod = num3 % num4;
